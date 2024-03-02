5c6e8bf5871a9cbd4afeb760d4a9f81f0c847d33
Victor <warfaredev@gmail.com>
2024-03-02 14:41:58 +0400

    Create compile_g++.sh

---
+++ compile_g++.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+
+mkdir bin 2>/dev/null
+rm -f bin/rehlds_sq.so 2>/dev/null
+
+g++ -s -g0 -O3 -msse -msse2 -msse3 -msse4.1 -msse4.2 -fno-exceptions -fno-rtti -fno-stack-protector -m32 -fno-rtti -mcrc32 -fPIC -s -shared -static-libgcc \-Isrc \-Idep/hlsdk/common -Idep/hlsdk/dlls -Idep/hlsdk/engine -Idep/hlsdk/public \-Idep/metamod \-Idep/steam \src/*.cpp \-o bin/gcc/rehlds_sq.so
+	
+
